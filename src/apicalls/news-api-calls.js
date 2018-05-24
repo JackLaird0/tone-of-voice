3abd21b36d552b30314a229c0a34fd0551b540b3
Jack Laird <jack.laird0@gmail.com>
2018-05-24 13:34:47 -0600

    Make a fetch call for trending news

---
+++ src/apicalls/news-api-calls.js
@@ -0,0 +1,8 @@
+import apiKey from './news-api-key';
+
+export const fetchTrendingNews = async () => {
+  const url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`;
+  const response = await fetch(url);
+  const trendingNews = await response.json();
+  return trendingNews
+};
