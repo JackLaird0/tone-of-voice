c47f69ea424ff464d0dc6af225e12ad814d5394c
Jack Laird <jack.laird0@gmail.com>
2018-05-28 21:50:14 -0600

    Make fetch call for CNN news

--- src/apicalls/news-api-calls.js
+++ src/apicalls/news-api-calls.js
@@ -14,9 +14,23 @@ export const fetchABCNews = async () => {
   return ABCNews;
 };
 
-export const getchBBCNews = async () => {
+export const fetchBBCNews = async () => {
   const url = `https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=${apiKey}`;
   const response = await fetch(url);
   const BBCNews = await response.json();
   return BBCNews;
 };  
+
+export const fetchCBSNews = async () => {
+  const url = `https://newsapi.org/v2/top-headlines?sources=cbs-news&apiKey=${apiKey}`;
+  const response = await fetch(url);
+  const CBSNews = await response.json();
+  return CBSNews;
+};
+
+export const fetchCNNNews = async () => {
+  const url = `https://newsapi.org/v2/top-headlines?sources=cnn&apiKey=${apiKey}`;
+  const response = await fetch(url);
+  const CNNNews = await response.json();
+  return CNNNews;
+};
