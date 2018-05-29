301955a2e1128aebfeea78fc71f9e6cc1d6c3570
Jack Laird <jack.laird0@gmail.com>
2018-05-29 00:49:27 -0600

    Create header component in app

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Route } from 'react-router-dom';
-import articles from './containers/articles/articles'
+import Articles from './containers/articles/Articles';
+import { Header } from './components/header/Header'
 
 class App extends Component {
   render() {
     return (
-      <Route path='/' component={articles} />
+      <div>
+        <Route path='/' component={Header} />
+        <Route exact path='/' component={Articles} />
+      </div>
     );
   }
 }
