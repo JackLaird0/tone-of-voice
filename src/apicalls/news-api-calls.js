ef4b190fb98f4a9c68cdb6ed2ef317f17b783d66
Jack Laird <jack.laird0@gmail.com>
2018-05-29 18:22:26 -0600

    Make api calls one function that takes the argument of the URL

--- src/apicalls/news-api-calls.js
+++ src/apicalls/news-api-calls.js
@@ -1,50 +1,5 @@
-import { apiKey } from './news-api-key';
-
-export const fetchTrendingNews = async () => {
-  const url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`;
+export const fetchNewsArticles = async (url) => {
   const response = await fetch(url);
-  const trendingNews = await response.json();
-  return trendingNews;
-};
-
-export const fetchABCNews = async () => {
-  const url = `https://newsapi.org/v2/top-headlines?sources=abc-news&apiKey=${apiKey}`;
-  const response = await fetch(url);
-  const ABCNews = await response.json();
-  return ABCNews;
-};
-
-export const fetchBBCNews = async () => {
-  const url = `https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=${apiKey}`;
-  const response = await fetch(url);
-  const BBCNews = await response.json();
-  return BBCNews;
-};  
-
-export const fetchCBSNews = async () => {
-  const url = `https://newsapi.org/v2/top-headlines?sources=cbs-news&apiKey=${apiKey}`;
-  const response = await fetch(url);
-  const CBSNews = await response.json();
-  return CBSNews;
-};
-
-export const fetchCNNNews = async () => {
-  const url = `https://newsapi.org/v2/top-headlines?sources=cnn&apiKey=${apiKey}`;
-  const response = await fetch(url);
-  const CNNNews = await response.json();
-  return CNNNews;
-};
-
-export const fetchWashingtonPostNews = async () => {
-  const url = `https://newsapi.org/v2/top-headlines?sources=the-washington-post&apiKey=${apiKey}`;
-  const response = await fetch(url);
-  const WashingtonPostNews = await response.json();
-  return WashingtonPostNews;
-};
-
-export const fetchFoxNews = async () => {
-  const url = `https://newsapi.org/v2/top-headlines?sources=fox-news&apiKey=${apiKey}`;
-  const response = await fetch(url);
-  const FoxNews = await response.json();
-  return FoxNews;
+  const articles = await response.json();
+  return articles;
 };
