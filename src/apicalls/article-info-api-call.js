54dfc652e3b29683dde4c937df375f280ebbeb99
Jack Laird <jack.laird0@gmail.com>
2018-06-01 19:59:23 -0600

    Add try catch error handling

--- src/apicalls/article-info-api-call.js
+++ src/apicalls/article-info-api-call.js
@@ -1,8 +1,12 @@
 import { apiKey } from './article-info-api-key';
 
 export const fetchArticleInfo = async (articleUrl) => {
-  const url = `https://api.diffbot.com/v3/article?token=${apiKey}&url=${articleUrl}`
-  const response = await fetch(url);
-  const articleInfo = await response.json();
-  return articleInfo;
+  try {
+    const url = `https://api.diffbot.com/v3/article?token=${apiKey}&url=${articleUrl}`
+    const response = await fetch(url);
+    const articleInfo = await response.json();
+    return articleInfo;
+  } catch (error) {
+    throw error
+  }
 };
--- src/apicalls/news-api-calls.js
+++ src/apicalls/news-api-calls.js
@@ -1,5 +1,10 @@
 export const fetchNewsArticles = async (url) => {
-  const response = await fetch(url);
-  const articles = await response.json();
-  return articles;
+  try {
+    const response = await fetch(url);
+    const articles = await response.json();
+    return articles;
+  } catch (error){
+    throw error
+  }
 };
+
