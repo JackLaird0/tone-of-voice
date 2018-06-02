1bbf6d175f9ae9d53fb68115f74b800e751f652c
Jack Laird <jack.laird0@gmail.com>
2018-06-02 16:55:34 -0600

    Write tests for header container

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Header.css';
 import { NavLink } from 'react-router-dom';
 import { connect } from 'react-redux';
@@ -8,65 +8,67 @@ import { ABCNews, BBCNews, CBSNews, CNNNews,
   WashPostNews, FoxNews } from './../../apicalls/api-call-urls';
 import { Link } from 'react-router-dom'
 
-const Header = (props) => {
+export class Header extends Component {
 
-  const selectCategory = async (url, outlet) => {
-    if(!props.news[outlet].length) {
+  selectCategory = async (url, outlet) => {
+    if(!this.props.news[outlet].length) {
       const news = await fetchNewsArticles(url);
-      props.addNews(outlet, news.articles);
+      this.props.addNews(outlet, news.articles);
     }
-    props.changeSelectedOutlet(outlet);
+    this.props.changeSelectedOutlet(outlet);
   }
 
-  return (
-    <div className='header-container'>
-      <h1> tone of voice </h1>
-      <header className='header'>
-        <Link to='/'>
-          <button className='home' onClick={() => { props.changeSelectedOutlet('trending') }}>
-            TRENDING
-          </button>
-        </Link>
-        <Link to='/'> 
-          <button className='fox' onClick={() => { selectCategory(FoxNews, 'fox') }}>
-            FOX
-          </button>
-        </Link>
-        <Link to='/'> 
-          <button className='abc' onClick={() => { selectCategory(ABCNews, 'abc') }}>
-            ABC
-          </button>
-        </Link>
-        <Link to='/'> 
-          <button className='bbc' onClick={() => { selectCategory(BBCNews, 'bbc') }}>
-            BBC
-          </button>
-        </Link>
-        <Link to='/'> 
-          <button className='cnn' onClick={() => { selectCategory(CNNNews, 'cnn') }}>
-            CNN
-          </button>
-        </Link>
-        <Link to='/'> 
-          <button className='cbs' onClick={() => { selectCategory(CBSNews, 'cbs') }}>
-            CBS
-          </button>
-        </Link>
-        <Link to='/'> 
-          <button className='washPost' onClick={() => { selectCategory(WashPostNews, 'washPost') }}>
-            WASH POST
-          </button>
-        </Link>
-      </header>
-    </div>
-  )
+  render() {
+    return (
+      <div className='header-container'>
+        <h1> tone of voice </h1>
+        <header className='header'>
+          <Link to='/'>
+            <button className='home' onClick={() => { this.props.changeSelectedOutlet('trending') }}>
+              TRENDING
+            </button>
+          </Link>
+          <Link to='/'> 
+            <button className='fox' onClick={() => { this.selectCategory(FoxNews, 'fox') }}>
+              FOX
+            </button>
+          </Link>
+          <Link to='/'> 
+            <button className='abc' onClick={() => { this.selectCategory(ABCNews, 'abc') }}>
+              ABC
+            </button>
+          </Link>
+          <Link to='/'> 
+            <button className='bbc' onClick={() => { this.selectCategory(BBCNews, 'bbc') }}>
+              BBC
+            </button>
+          </Link>
+          <Link to='/'> 
+            <button className='cnn' onClick={() => { this.selectCategory(CNNNews, 'cnn') }}>
+              CNN
+            </button>
+          </Link>
+          <Link to='/'> 
+            <button className='cbs' onClick={() => { this.selectCategory(CBSNews, 'cbs') }}>
+              CBS
+            </button>
+          </Link>
+          <Link to='/'> 
+            <button className='washPost' onClick={() => { this.selectCategory(WashPostNews, 'washPost') }}>
+              WASH POST
+            </button>
+          </Link>
+        </header>
+      </div>
+    )
+  }
 }
 
-const mapStateToProps = state => ({
+export const mapStateToProps = state => ({
   news: state.news
 })
 
-const mapDispatchToProps = dispatch => ({
+export const mapDispatchToProps = dispatch => ({
   changeSelectedOutlet: (outlet) => dispatch(changeSelectedOutlet(outlet)),
   addNews: (outlet, news) => dispatch(addNews(outlet, news))
 })
