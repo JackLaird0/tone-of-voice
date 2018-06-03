1154a2cbbb23a6aefc2ac46e2a4beef38502a99c
Jack Laird <jack.laird0@gmail.com>
2018-06-02 23:27:46 -0600

    Create add tone data reducer

---
+++ src/reducers/add-tone-data-reducer.js
@@ -0,0 +1,9 @@
+export const addToneDataReducer = (state = {}, action) => {
+  switch (action.type) {
+    case 'ADD_TONE_DATA':
+      const newState = Object.assign({}, state, {[action.name]: action.tone})
+      return newState;
+    default:
+      return state
+  }
+}
