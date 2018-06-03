43a3855d342bf0554c418616bb483cbaa7456326
Jack Laird <jack.laird0@gmail.com>
2018-06-02 20:23:43 -0600

    Make watson api call

---
+++ src/apicalls/watson-tone-api-call.js
@@ -0,0 +1,20 @@
+export const fetchWatsonAnalysis = async (text) => {
+  try {
+    const url = 'http://localhost:5000/api/v1/analysis';
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify({'text': text})
+    })
+    
+    if (response.status <= 200){
+      return await response.json()
+    } else {
+      throw new Error("Unable to get text's tone")
+}
+  } catch (error) {
+    throw error
+  }
+}
