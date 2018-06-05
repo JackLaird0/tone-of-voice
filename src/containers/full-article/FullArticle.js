9ac169e37146390133a6ee4ce3e09b8210d0ef13
Jack Laird <jack.laird0@gmail.com>
2018-06-05 17:58:11 -0600

    Update styling of FullArticle container

--- src/containers/full-article/FullArticle.js
+++ src/containers/full-article/FullArticle.js
@@ -1,22 +1,27 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import './FullArticle.css'
 
 export class FullArticle extends Component {
 
   displayArticle = () => {
     if (!Object.keys(this.props.article).length) {
       return (
-        <img src="http://profanderson.blog.etecarmine.com.br/wp-content/uploads/2017/10/loading-gif-transparent-10.gif" alt="loading gif"/>
+        <img className='loading 'src="http://profanderson.blog.etecarmine.com.br/wp-content/uploads/2017/10/loading-gif-transparent-10.gif" alt="loading gif"/>
       )
     } else {
       const {title, author, text, pageUrl} = this.props.article;
-      return (
-        <div>
-          <h2>{title}</h2>
-          <h5>{author}</h5>
-          <p>{text}</p>
-          <a href={pageUrl} target='_blank'> View Article</a>
+      const { url } = this.props.article.images[0];
+      return ( 
+        <div className='container'>
+          <img src={url} alt='article-image' className='image'/>
+          <div className='text-container'>
+            <h2 className='article-title'>{title}</h2>
+            <h5 className='article-author'>{author}</h5>
+            <p className='article-text'>{text}</p>
+          </div>
+          <a href={pageUrl} target='_blank' className='article-link'> View Article</a>
         </div>
       );
     }
